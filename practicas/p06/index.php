7ffead76f64e2838549e215a9b7db480da066c08
gabo10-FS <ausaldivar568@gmail.com>
2024-09-20 11:29:41 -0600

    Modificación del title de index.php

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Práctica 4</title>
+    <title>Práctica 6</title>
 </head>
 <body>
     <h2>Ejercicio 1</h2>
