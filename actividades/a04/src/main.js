399279ac545fd8570efc40615bbae1f09c134cd9
gabo10-FS <ausaldivar568@gmail.com>
2024-10-08 19:34:06 -0600

    Estructura y ejemplos 01, 02, y 03 de a04

---
+++ actividades/a04/src/main.js
@@ -0,0 +1,94 @@
+function getDatos(){
+    var nombre = prompt('Nombre: ','');
+    var edad = prompt('Edad: ',0);
+
+    var div1 = document.getElementById('nombre');
+    div1.innerHTML = '<h3>Nombre: ' + nombre + '</h3>';
+    var div2 = document.getElementById('edad');
+    div2.innerHTML = '<h3>Edad: ' + edad + '</h3>';
+}
+
+function ejemplo01(){
+    var div = document.getElementById('ejemplo01');
+    div.innerHTML = '<h3>Hola Mundo</h3>';
+}
+
+function ejemplo02(){
+    var nombre = 'Angel';
+    var edad = 20;
+    var altura = 1.72;
+    var casado = false;
+    var div = document.getElementById('ejemplo02');
+    div.innerHTML = '<h3>Nombre: ' + nombre + '</h3>'
+    + '<h3>Edad: ' + edad + '</h3>'
+    + '<h3>Altura: ' + altura + '</h3>'
+    + '<h3>Casado: ' + casado + '</h3>';
+}
+function ejemplo03(){
+    var nombre;
+    var edad;
+    nombre = prompt('Ingresa tu nombre:', '');
+    edad = prompt('Ingresa tu edad:', '');
+    var div = document.getElementById('ejemplo03');
+    div.innerHTML = '<h3>Hola ' + nombre + ' así que tienes ' + edad + ' años';
+}
+
+function ejemplo04(){
+    var div = document.getElementById('ejemplo04');
+}
+
+function ejemplo05(){
+    var div = document.getElementById('ejemplo05');
+}
+
+function ejemplo06(){
+    var div = document.getElementById('ejemplo06');
+}
+
+function ejemplo07(){
+    var div = document.getElementById('ejemplo07');
+}
+
+function ejemplo08(){
+    var div = document.getElementById('ejemplo08');
+}
+
+function ejemplo09(){
+    var div = document.getElementById('ejemplo09');
+}
+
+function ejemplo10(){
+    var div = document.getElementById('ejemplo10');
+}
+
+function ejemplo11(){
+    var div = document.getElementById('ejemplo11');
+}
+
+function ejemplo12(){
+    var div = document.getElementById('ejemplo12');
+}
+
+function ejemplo13(){
+    var div = document.getElementById('ejemplo13');
+}
+
+function ejemplo14(){
+    var div = document.getElementById('ejemplo14');
+}
+
+function ejemplo15(){
+    var div = document.getElementById('ejemplo15');
+}
+
+function ejemplo16(){
+    var div = document.getElementById('ejemplo16');
+}
+
+function ejemplo17(){
+    var div = document.getElementById('ejemplo17');
+}
+
+function ejemplo18(){
+    var div = document.getElementById('ejemplo18');
+}
