ff2b4a2a5cab2ef83d9f7dbaed75fc3a400da35c
gabo10-FS <ausaldivar568@gmail.com>
2024-10-11 02:27:53 -0600

    Ejercicio 3: boton editar y llenado de formulario. de p09

---
+++ practicas/p09/formulario_productos_v2.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 9</title>
+    <script src="validar_formulario_producto.js" defer></script>
+</head>
+<body>
+    <h1>Actualización de productos en almacén</h1>
+
+    <form id="formularioProductos" onsubmit="" method="post">
+
+    <h2>Producto</h2>
+    <fieldset>
+        <legend>Información propia del producto</legend>
+
+        <ul>
+            <li><label for="form-nombre">Nombre:</label> <input type="text" name="nombre" id="form-nombre" value="<?= !empty($_POST['nombre'])?$_POST['nombre']:$_GET['nombre'] ?>"></li>
+            <p>Marca: 
+                <select name="marca" id="form-marca">
+                    <option value="0"></option>
+                    <option value="APPLE" <?= isset($_POST['marca']) && $_POST['marca'] === 'APPLE' ? 'selected' : '' ?>>APPLE</option>
+                    <option value="HUAWEI" <?= isset($_POST['marca']) && $_POST['marca'] === 'HUAWEI' ? 'selected' : '' ?>>HUAWEI</option>
+                        <option value="LG" <?= isset($_POST['marca']) && $_POST['marca'] === 'LG' ? 'selected' : '' ?>>LG</option>
+                        <option value="MSI" <?= isset($_POST['marca']) && $_POST['marca'] === 'MSI' ? 'selected' : '' ?>>MSI</option>
+                        <option value="DELL" <?= isset($_POST['marca']) && $_POST['marca'] === 'DELL' ? 'selected' : '' ?>>DELL</option>
+                        <option value="SAMSUNG" <?= isset($_POST['marca']) && $_POST['marca'] === 'SAMSUNG' ? 'selected' : '' ?>>SAMSUNG</option>
+                        <option value="LENOVO" <?= isset($_POST['marca']) && $_POST['marca'] === 'LENOVO' ? 'selected' : '' ?>>LENOVO</option>
+                        <option value="ASUS" <?= isset($_POST['marca']) && $_POST['marca'] === 'ASUS' ? 'selected' : '' ?>>ASUS</option>
+                        <option value="HP" <?= isset($_POST['marca']) && $_POST['marca'] === 'HP' ? 'selected' : '' ?>>HP</option>
+                        <option value="ACER" <?= isset($_POST['marca']) && $_POST['marca'] === 'ACER' ? 'selected' : '' ?>>ACER</option>
+                        <option value="SONY" <?= isset($_POST['marca']) && $_POST['marca'] === 'SONY' ? 'selected' : '' ?>>SONY</option>
+                        <option value="TOSHIBA" <?= isset($_POST['marca']) && $_POST['marca'] === 'TOSHIBA' ? 'selected' : '' ?>>TOSHIBA</option>
+                </select>
+            </p>
+            <li><label for="form-modelo">Modelo:</label> <input type="text" name="modelo" id="form-modelo" value="<?= !empty($_POST['modelo'])?$_POST['modelo']:$_GET['modelo'] ?>"></li>
+            <li><label for="form-descripcion">Descripción</label><br><textarea name="descripcion" rows="4" cols="60" id="form-descripcion" placeholder="No más de 300 caracteres de longitud"><?= !empty($_POST['descripcion'])?$_POST['descripcion']:$_GET['descripcion'] ?></textarea></li>
+        </ul>
+    </fieldset>
+
+    <h2>Almacén</h2>
+
+    <fieldset>
+        <legend>Información del producto en tienda</legend>
+
+        <ul>
+            <li><label for="form-precio">Precio:</label> <input type="number" name="precio" id="form-precio" step="0.01" value="<?= !empty($_POST['precio'])?$_POST['precio']:$_GET['precio'] ?>"></li>
+            <li><label for="form-unidad">Unidades</label> <input type="number" name="unidad" id="form-unidad" value="<?= !empty($_POST['unidad'])?$_POST['unidad']:$_GET['unidad'] ?>"></li>
+            <li><label for="form-img">Ruta de la imagen</label> <input type="text" name="img" id="form-img" value="<?= !empty($_POST['img'])?$_POST['img']:$_GET['img'] ?>"></li>
+        </ul>
+    </fieldset>
+
+    <p>
+        <input type="submit" value="Actualizar">
+    </p>
+
+    </form>
+</body>
+</html>
---
+++ practicas/p09/get_productos_vigentes_v2.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<?php
+    //header("Content-Type: application/json; charset=utf-8"); 
+    
+	/** SE CREA EL OBJETO DE CONEXION */
+    @$link = new mysqli('localhost', 'root', 'gaboas', 'marketzone');
+    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+
+    /** comprobar la conexión */
+    if ($link->connect_errno) 
+    {
+        die('Falló la conexión: '.$link->connect_error.'<br/>');
+        //exit();
+    }
+
+    /** Crear una tabla que no devuelve un conjunto de resultados */
+    if ( $result = $link->query("SELECT * FROM productos WHERE eliminado = 0") ) 
+    {
+        /** Se extraen las tuplas obtenidas de la consulta */
+        $row = $result->fetch_all(MYSQLI_ASSOC);
+
+        /** Se crea un arreglo con la estructura deseada */
+        foreach($row as $num => $registro) {            // Se recorren tuplas
+            foreach($registro as $key => $value) {      // Se recorren campos
+                $data[$num][$key] = utf8_encode($value);
+            }
+        }
+
+        /** útil para liberar memoria asociada a un resultado con demasiada información */
+        $result->free();
+    }
+
+    $link->close();
+
+    /** Se devuelven los datos en formato JSON */
+    //echo json_encode($data, JSON_PRETTY_PRINT);
+	?>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Producto</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<script>
+            function getData() {
+                // se obtiene el id de la fila donde está el botón presinado
+                var rowId = event.target.parentNode.parentNode.id;
+
+                // se obtienen los datos de la fila en forma de arreglo
+                var data = document.getElementById(rowId).querySelectorAll(".row-data");
+                /**
+                querySelectorAll() devuelve una lista de elementos (NodeList) que 
+                coinciden con el grupo de selectores CSS indicados.
+                (ver: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Selectors)
+
+                En este caso se obtienen todos los datos de la fila con el id encontrado
+                y que pertenecen a la clase "row-data".
+                */
+
+                var name = data[0].innerHTML;
+				var marca = data[1].innerHTML;
+				var modelo = data[2].innerHTML;
+                var desc = data[5].innerHTML;
+				var precio = data[3].innerHTML;
+				var unid = data[4].innerHTML;
+                var img = data[6].getAttribute('data-img');;
+
+                send2form(name, marca, modelo, desc, precio, unid, img);
+            }
+        </script>
+	</head>
+	<body>
+		<h3>PRODUCTO</h3>
+
+		<br/>
+		
+		<?php if( isset($row) ) : ?>
+			<table class="table">
+				<thead class="thead-dark">
+					<tr>
+					<th scope="col">#</th>
+					<th scope="col">Nombre</th>
+					<th scope="col">Marca</th>
+					<th scope="col">Modelo</th>
+					<th scope="col">Precio</th>
+					<th scope="col">Unidades</th>
+					<th scope="col">Detalles</th>
+					<th scope="col">Imagen</th>
+					<th scope="col">Modificar</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach($row as $value) : ?>
+					<tr id="<?= $value['id'] ?>">
+						<th scope="row"><?= $value['id'] ?></th>
+						<td class="row-data"><?= $value['nombre'] ?></td>
+						<td class="row-data"><?= $value['marca'] ?></td>
+						<td class="row-data"><?= $value['modelo'] ?></td>
+						<td class="row-data"><?= $value['precio'] ?></td>
+						<td class="row-data"><?= $value['unidades'] ?></td>
+						<td class="row-data"><?= $value['detalles'] ?></td>
+						<td class="row-data" data-img="<?= $value['imagen'] ?>"><img src=<?= $value['imagen'] ?> ></td>
+						<td><input type="button" value="Editar" onclick="getData()" /></td>
+					</tr>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
+		<?php elseif(!empty($id)) : ?>
+
+			 <script>
+                alert('El ID del producto no existe');
+             </script>
+
+		<?php endif; ?>
+		<script>
+            function send2form(name, marca, modelo, desc, precio, unid, img) {
+                var form = document.createElement("form");
+
+                var nombreIn = document.createElement("input");
+                nombreIn.type = 'text';
+                nombreIn.name = 'nombre';
+                nombreIn.value = name;
+                form.appendChild(nombreIn);
+
+                var marcaIn = document.createElement("select");
+                marcaIn.name = 'marca';
+                var options = ["APPLE", "HUAWEI", "LG", "MSI", "DELL", "SAMSUNG", "LENOVO", "ASUS", "HP", "ACER", "SONY", "TOSHIBA"];
+            	options.forEach(function(option) {
+                var opt = document.createElement("option");
+                opt.value = option;
+                opt.textContent = option;
+                if (option === marca) {
+                    opt.selected = true; // opción correspondiente
+                }
+                marcaIn.appendChild(opt);
+            });
+                form.appendChild(marcaIn);
+
+				var modeloIn = document.createElement("input");
+                modeloIn.type = 'text';
+                modeloIn.name = 'modelo';
+                modeloIn.value = modelo;
+                form.appendChild(modeloIn);
+
+                var precioIn = document.createElement("input");
+                precioIn.type = 'number';
+                precioIn.name = 'precio';
+                precioIn.value = precio;
+                form.appendChild(precioIn);
+
+				var unidIn = document.createElement("input");
+                unidIn.type = 'number';
+                unidIn.name = 'unidad';
+                unidIn.value = unid;
+                form.appendChild(unidIn);
+
+				var descIn = document.createElement("input");
+                descIn.type = 'text';
+                descIn.name = 'descripcion';
+                descIn.value = desc;
+                form.appendChild(descIn);
+
+				var imgIn = document.createElement("input");
+                imgIn.type = 'text';
+                imgIn.name = 'img';
+                imgIn.value = img;
+                form.appendChild(imgIn);	
+
+                form.method = 'POST';
+                form.action = 'http://localhost/tecweb/practicas/p09/formulario_productos_v2.php';  
+
+                document.body.appendChild(form);
+                form.submit();
+            }
+        </script>
+	</body>
+</html>
